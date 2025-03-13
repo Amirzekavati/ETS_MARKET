4bc6b89b8826406a2b76c2e8e4bf13ffdcb488c0
Amirz <amir.zekavati.a@gmail.com>
2025-03-13 18:29:07 +0330

    config file is completed.

---
+++ config.py
@@ -0,0 +1,5 @@
+BASE_URL = "https://fxmarketrate.cbi.ir/"
+HEADERS = {
+    "user-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+}
+OUTPUT_FILE = "data/data.csv"

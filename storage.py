7e138bef87cfcf7f85c3fd4afed3d1bb9563b077
Amirz <amir.zekavati.a@gmail.com>
2025-03-13 19:54:04 +0330

    Handle saving to csv

--- storage.py
+++ storage.py
@@ -1,1 +1,10 @@
-import
+import pandas as pd
+import os
+import config
+
+def save_to_csv(data, filename=config.OUTPUT_FILE):
+    """Saves scaped data to CSV file."""
+    os.makedirs("data", exist_ok=True)
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False, mode='a', header=not os.path.exists(filename))
+    print(f"✅ Data saved to {filename}")

eef70dac51422a598ffad195091bb7e837999dcd
Amirz <amir.zekavati.a@gmail.com>
2025-03-13 19:15:30 +0330

    start to complete the select date

--- scraper.py
+++ scraper.py
@@ -1,11 +1,18 @@
 import config
 
 
+
 class Scraper:
     def __init__(self, url):
         self.url = url
         self.driver = webdriver.chrome()
         
     def open_website(self):
+        """Opens the target website."""
         self.driver.get(self.url)
+        
+    def select_date(self, date):
+        """Selects the given date in the website."""
+        try:
+            date_input = self.driver.find
         

27a39c73ead19035e69fe97c7c7403297bdaa8f2
Amirz <amir.zekavati.a@gmail.com>
2025-03-13 19:07:32 +0330

    add xpath to config file.

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 BASE_URL = "https://fxmarketrate.cbi.ir/"
-HEADERS = {
-    "user-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
-}
+DATE_INPUT_XPATH = "//*[@id="MainContent_ViewCashChequeRates_txtDate"]"
+DATE_SUBMIT_XPATH = "//*[@id="MainContent_ViewCashChequeRates_Button1"]"    
 OUTPUT_FILE = "data/data.csv"
